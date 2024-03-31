99d2286cfab84a9dc30c676f9d2f1b273d13979a
MergimHo <*Hansbunte22>
2024-03-31 13:43:13 +0200

    init

---
+++ main.py
@@ -0,0 +1,166 @@
+import uvicorn
+from starlette.responses import RedirectResponse, JSONResponse
+from fastapi import FastAPI, HTTPException, File, UploadFile, Query
+from typing import Optional, List
+from csv import reader
+from io import StringIO
+import logging as log
+
+
+### gloabls
+app = FastAPI()
+indices = []
+valid_indices = ["DAX" , "SP500", "ALL"]
+log.basicConfig(level=log.DEBUG)
+
+@app.get("/health")
+async def health():
+  log.info("Executing Health Check.")
+  return JSONResponse(status_code=200, content={"message": "Application is running."})
+
+@app.get("/")
+async def redirect():
+  response = RedirectResponse(url='/docs')
+  return response
+
+@app.get("/getdata/{date}")
+async def read_item(date: str, index: str = "DAX", show_all_indices: Optional[bool] = False):
+  result = {"date": date}
+
+  if date not in fake_items_db:
+        raise HTTPException(status_code=404, detail="Date not found")
+
+  elif show_all_indices:
+    for i in indices:
+      result.update({i: fake_items_db[date][i]})
+
+  else:
+    result.update({index: fake_items_db[date][index]})
+
+  return result
+
+
+"""
+Exercise 1: Get All Data By Index separated page by page with 30 entries by page
+Input parameter: 'DAX', 'SP500' or 'ALL'.
+"""
+@app.get("/dataByIndex/")
+async def read_item(index: str = Query("ALL", description="Index parameter, either 'DAX', 'SP500' or 'ALL'.")):
+
+  index = index.upper() if index else None
+
+  if index not in valid_indices or index is None:
+    log.error(f"Unknown parameter '{index}' was given. Valid are 'DAX', 'SP500' and 'ALL'.")
+    raise HTTPException(status_code=400, detail="Invalid Index parameter. Please choose 'DAX', 'SP500' or 'ALL'.")
+
+
+  log.info(f"Searching and Paginating for index {index}")
+  result = []
+  page_number = 1
+  page = {page_number: []}
+
+  for indice_counter, (date, indices) in enumerate(fake_items_db.items(), start=1):
+
+    current_share = (date, indices if index == "ALL" else indices.get(index))
+    page[page_number].append(current_share)
+
+    if indice_counter % 30 == 0 or indice_counter == len(fake_items_db):
+      result.append(page)
+      page_number = page_number + 1
+      page = {page_number: []}
+
+  log.info("Request was succesfull.") 
+  return JSONResponse(status_code=200, content=result, headers={"requested-index": index})
+
+"""
+Exercise 1: Get All Data separated page by page with 30 entries by page
+"""
+@app.get("/dataAll")
+async def read_item_all():
+  log.info("Redirecting to /dataByIndex with parameter 'ALL'")
+  return RedirectResponse(url='/dataByIndex?index=ALL')
+
+@app.get("/fullDatabase")
+async def get_full_database():
+  return JSONResponse(status_code=200, content=fake_items_db, headers={"requested-index": "full-data-base"})
+
+
+@app.post("/uploadfile/")
+async def create_upload_files(file: UploadFile = File(...)):
+  # add the entries of the uploaded file to fake_items_db
+  # data structure of fake_items_db:
+  # {"09091990": {"DAX": "1232456", "SP500:" "9238748"}, "08081990":{"DAX": "2345646", "SP500:" "2324432"}, ..}
+  global fake_items_db
+
+  if not file or file.size == 0:
+      log.error("File empty.")
+      raise HTTPException(status_code=400, detail="No file uploaded.")
+  elif file.content_type not in ['application/vnd.ms-excel', 'text/csv'] or not file.filename.lower().endswith('.csv'):
+    log.error("No CSV data reiceived")
+    raise HTTPException(415, "Only CSV data allowed. File ending must be *.csv and correct content-type.")
+
+  log.info(f"Received uploaded file {file.filename}. Now trying to update.")
+
+  try: 
+    content = await file.read()
+    contents_io = StringIO(content.decode('utf-8-sig'))
+    csv_reader = reader(contents_io)
+    csv_data = [row for row in csv_reader]
+    add_entries_to_dict(csv_data)
+    fake_items_db = dict(sorted(fake_items_db.items(), reverse=True))
+    log.info
+  except Exception as e:
+        log.error("File was uploaded and a CSV but its content could not be processed due to invalid entries.")
+        return JSONResponse(status_code=400, content={"message": "The request contained non-valid CSV data."})
+
+  return JSONResponse(status_code=201, content={"message": "The Shared Indices were succesfully updated."})
+
+
+def add_entries_to_dict(data):
+  """
+  Adds the entries to the global fake_items_db dictionairy.
+  Only available during server runtime.
+
+  Args:
+      data (list(list)): [["Date", "DAX", "SP501"], ["30112021", "132345", "762190"], ..]
+  """
+  for line_no, line in enumerate(data, 0):
+    if (line_no == 0):
+      new_indices = line[1:]
+      for index in new_indices:
+        indices.append(index) if index not in indices else indices
+        #todo: check order of line of line_no == 0 and indices
+    else:
+      fake_items_db.update({line[0]: {}})
+      for index_no, index in enumerate(indices, 1):
+        fake_items_db[line[0]].update({index: line[index_no + 0]})
+
+
+def initialize_dict(fake_items_db, filename):
+  """
+  Initializes the fake_items_db dictionairy
+
+  Args:
+      fake_items_db (dict): initial empty dictionairy
+      filename (string): path to inital file
+
+  Returns:
+      dict(dict): {"09091990": {"DAX": "1232456", "SP500:" "9238748"}, "08081990":{"DAX": "2345646", "SP500:" "2324432"} ..}
+  """
+  file = open('./' + filename, encoding='UTF8')
+  csv_reader = reader(file)
+  add_entries_to_dict(csv_reader)
+  file.close()
+
+  return fake_items_db
+
+# Helper method for testing purposes.
+def get_fake_items_db():
+  return fake_items_db
+
+fake_items_db = dict()
+fake_items_db = initialize_dict(fake_items_db=fake_items_db, filename="daxsp.csv")
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
---
+++ test_main.py
@@ -0,0 +1,243 @@
+import pytest
+from fastapi.testclient import TestClient
+from datetime import datetime, timedelta
+import random
+
+from main import app, get_fake_items_db
+
+
+# GLOBAL SETUP VARIABLES
+START_DATE = '01.01.2018'
+
+SIMPLE_TEST_DATA = {
+        '20150501': {'DAX': '1', 'SP500': '600'},
+        '20150502': {'DAX': '2', 'SP500': '1200'},
+        }
+
+# GLOBAL TEST SETUP
+
+@pytest.fixture
+def client():
+    return TestClient(app)
+
+
+# First example test.
+def test_redirect(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.text.find("Swagger") != -1
+    assert 1 == 1
+
+
+"""
+TESTS FOR EXERCISE 1
+"""
+
+# test helper function, bc its essential that it works and it contains some complexity.
+def test_helper_function():
+    # 60 days from 01.01.2018 is the 01.03.2018
+    number_of_expected_entries = 60
+    data = generate_test_data(number_of_expected_entries, START_DATE)
+    assert data.get('20180301') is not None
+    assert data.get('20180302') is None
+
+
+# /getDataAll with invalid index (not 'DAX' or 'SP500') should return HTTPException
+def test_invalid_index_for_getAll(client):
+    ERROR_MSG = "Invalid Index parameter. Please choose 'DAX', 'SP500' or 'ALL'."
+    exception_response = client.get("/dataByIndex?index=INVALID_INDEX")
+    assert exception_response.status_code == 400
+    assert exception_response.json()["detail"] == ERROR_MSG
+
+
+# /getDataAll with null should return 200 and the 'ALL' information in its header.
+def test_null_index_for_getAll_should_return_all(client):
+    exception_response = client.get("/dataByIndex")
+    assert exception_response.status_code == 200
+    assert exception_response.headers["requested-index"] == "ALL"
+
+
+# /getDataAll should return just the index of 'DAX' explicit
+def test_values_match_dax_explicit(client, mocker):
+    dax_values = ['1', '2']
+    mocker.patch('main.fake_items_db', SIMPLE_TEST_DATA)
+
+    dax_reponse = client.get("/dataByIndex?index=DAX")
+    body = dax_reponse.json()
+    body_values = [indice[1] for page in body for indices in page.values() for indice in indices]
+    assert body_values == dax_values
+
+
+# /getDataAll should return just the index of 'DAX'
+def test_index_DAX_should_not_contain_SP500(client, mocker):
+    data = generate_test_data(number_of_entries=20, start_date=START_DATE)
+    mocker.patch('main.fake_items_db', data)
+
+    dax_reponse = client.get("/dataByIndex?index=DAX")
+    body = dax_reponse.json()
+
+    dax_values = [value['DAX'] for value in data.values()]
+    body_values = [indice[1] for page in body for indices in page.values() for indice in indices]
+    assert body_values == dax_values
+
+
+# /getDataAll should return just the index of 'DAX' explicit
+def test_values_match_sp500_explicit(client, mocker):
+    sp500_values = ['600', '1200']
+    mocker.patch('main.fake_items_db', SIMPLE_TEST_DATA)
+
+    sp500_response = client.get("/dataByIndex?index=SP500")
+    body = sp500_response.json()
+    body_values = [indice[1] for page in body for indices in page.values() for indice in indices]
+    assert body_values == sp500_values
+
+
+# /getDataAll should reuturn just the index of 'SP500'
+def test_index_SP500_should_not_contain_DAX(client, mocker):
+    data = generate_test_data(number_of_entries=20, start_date=START_DATE)
+    mocker.patch('main.fake_items_db', data)
+
+    dax_reponse = client.get("/dataByIndex?index=SP500")
+    body = dax_reponse.json()
+
+    sp500_values = [value['SP500'] for value in data.values()]
+    body_values = [indice[1] for page in body for indices in page.values() for indice in indices]
+    assert body_values == sp500_values
+
+
+"""
+Here we craete a fake_db with a certain amount of entries and the expected pages.
+We test /dataByIndex and /dataAll here together because focus in this test on page numbers.
+"""
+@pytest.mark.parametrize("num_of_entries, expected_pages", [
+    (0, 0), (29, 1), (30, 1), (31, 2), (60, 2),
+    (61, 3), (88, 3), (89, 3), (90, 3), (91, 4),
+    (1412, 48)
+])
+def test_correct_page_numbers_by_index(client, mocker, num_of_entries, expected_pages):
+    data = generate_test_data(number_of_entries=num_of_entries, start_date=START_DATE)
+    mocker.patch('main.fake_items_db', data)
+
+    response_by_index = client.get("/dataByIndex?index=DAX")
+
+    assert response_by_index.status_code == 200, "Status code was not 200 as expected"
+    assert len(response_by_index.json()) == expected_pages, f"Page number was {len(response_by_index.json())} not as expected {expected_pages}"
+
+    response_all = client.get("/dataAll")
+
+    assert response_all.status_code == 200, "Status code was not 200 as expected"
+    assert len(response_all.json()) == expected_pages, f"Page number was {len(response_all.json())} not as expected {expected_pages}"
+
+
+"""
+TESTS FOR EXERCISE 2
+"""
+# POST of csv that is NULL
+def test_csv_is_null(client, mocker):
+    ERROR_MSG =  "No file uploaded."
+    mocker.patch('main.fake_items_db', SIMPLE_TEST_DATA)
+
+    empty_bytes = b''
+
+    post_request = client.post('/uploadfile/', files={"file": (f"daxsp_c.csv", empty_bytes)})
+    assert post_request.status_code == 400
+    assert post_request.json()["detail"] == ERROR_MSG
+
+
+
+# POST if does not end with *.csv
+def test_csv_ends_not_with_csv(client, mocker):
+    FILE_ENDING = 'XXX'
+    ERROR_MSG =  "Only CSV data allowed. File ending must be *.csv and correct content-type."
+    mocker.patch('main.fake_items_db', SIMPLE_TEST_DATA)
+
+    with open("test_resources\daxsp_c.csv", "rb") as csv_file:
+        file_bytes = csv_file.read()
+
+    post_request = client.post('/uploadfile/', files={"file": (f"daxsp_c.{FILE_ENDING}", file_bytes)})
+    assert post_request.status_code == 415
+    assert post_request.json()["detail"] == ERROR_MSG
+
+
+# POST if content_type is  ['application/vnd.ms-excel', 'text/csv']
+def test_csv_correct_content_type(client, mocker):
+    ALLOWED_CONTENT_TYPE =  ['application/vnd.ms-excel', 'text/csv']
+    mocker.patch('main.fake_items_db', SIMPLE_TEST_DATA)
+
+    with open("test_resources\daxsp_c.csv", "rb") as csv_file:
+        file_bytes = csv_file.read()
+
+    for content_type in ALLOWED_CONTENT_TYPE:
+        post_request = client.post('/uploadfile/', files={"file": (f"daxsp_c.csv", file_bytes, content_type)})
+        assert post_request.status_code == 201
+
+
+# POST if content_type is not  ['application/vnd.ms-excel', 'text/csv']
+def test_csv_wrong_content_type(client, mocker):
+    WRONG_CONTENT_TYPE = "application/json"
+    ERROR_MSG =  "Only CSV data allowed. File ending must be *.csv and correct content-type."
+    mocker.patch('main.fake_items_db', SIMPLE_TEST_DATA)
+
+    with open("test_resources\daxsp_c.csv", "rb") as csv_file:
+        file_bytes = csv_file.read()
+
+    post_request = client.post('/uploadfile/', files={"file": (f"daxsp_c.csv", file_bytes, WRONG_CONTENT_TYPE)})
+    assert post_request.status_code == 415
+    assert post_request.json()["detail"] == ERROR_MSG
+
+
+# Check that entries were added. in SIMPLE_TEST_DATA we have 2 entries, we add 3 more. 
+def test_upload_csv_contains_3_entries(client, mocker):
+    EXPECTED_ENTRIES = 5
+    mocker.patch('main.fake_items_db', SIMPLE_TEST_DATA)
+
+    with open("test_resources\daxsp_c.csv", "rb") as csv_file:
+        file_bytes = csv_file.read()
+
+    post_request = client.post('/uploadfile/', files={"file": ("daxsp_c.csv", file_bytes)})
+    db = get_fake_items_db()
+    
+    assert post_request.status_code == 201
+    assert len(db) == EXPECTED_ENTRIES
+
+
+
+# Check that entries are sorted 
+def test_final_db_is_sorted(client, mocker):
+    mocker.patch('main.fake_items_db', SIMPLE_TEST_DATA)
+
+    with open("test_resources\daxsp_c.csv", "rb") as csv_file:
+        file_bytes = csv_file.read()
+
+    post_request = client.post('/uploadfile/', files={"file": ("daxsp_c.csv", file_bytes)})
+    dates_from_db = list(get_fake_items_db().keys())
+    formatted_dates_from_db = [datetime.strptime(date, "%Y%m%d") for date in dates_from_db]
+
+    assert all(formatted_dates_from_db[i] >= formatted_dates_from_db[i+1] for i in range(len(formatted_dates_from_db)-1))
+
+
+
+
+# HELPER FUNCTIONS
+
+"""
+This function will generate a dict that contains data in the format 
+{<DATE> : {'DAX' : <NUMBER> : 'SP500': <NUMBER>}} by the entries and a start date.
+The start date will increase by a day.
+Note that for readability we entered START_DATE as %d.%m.%Y.
+"""
+def generate_test_data(number_of_entries: int, start_date: str):
+    data = {}
+    current_date = datetime.strptime(start_date, '%d.%m.%Y')
+    for i in range(number_of_entries):
+        data[current_date.strftime('%Y%m%d')] = {
+            'DAX' : str(random.randint(1,1000)),
+            'SP500' : str(random.randint(1,1000))
+        }
+        current_date += timedelta(days=1)
+    
+    return data
+
+
+
+
